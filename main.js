8c1af55b291969934df5ef03a178ca3f56015f67
sxvxn <unormiesable@gmail.com>
2024-05-09 17:37:56 +0700

    Add 3D View

--- main.js
+++ main.js
@@ -1,1 +1,54 @@
 import './style.css'
+
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/Addons.js';
+
+// Canvas
+const canvas = document.querySelector('#main-viewport');
+const renderer = new THREE.WebGLRenderer({ canvas, alpha:true, antialias:true });
+renderer.setSize(canvas.clientWidth, canvas.clientHeight);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.shadowMap.autoUpdate = true;
+
+// Scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xAAAAAA);
+
+// Camera
+const main_camera = new THREE.PerspectiveCamera(50, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
+main_camera.position.set(4, 2, 4);
+
+// Controls (Orbit Controls)
+const controls = new OrbitControls(main_camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.03;
+controls.screenSpacePanning = false;
+controls.enablePan = false;
+controls.enableZoom = true;
+controls.maxDistance = 30;
+controls.minDistance = 1;
+
+// GRID
+const grid = new THREE.GridHelper(100, 100, '#FFFFFF', '#BBBBBB');
+grid.position.set(0, 0, 0);
+scene.add(grid);
+
+// RESIZE WINDOW 
+function onWindowResize() {
+    main_camera.aspect = window.innerWidth / window.innerHeight;
+    main_camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+  
+// MAIN FUNC
+function main() {
+    requestAnimationFrame(main);
+    renderer.render(scene, main_camera);
+    controls.update();
+}
+
+window.addEventListener('resize', onWindowResize);
+onWindowResize();
+main();
